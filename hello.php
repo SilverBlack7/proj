8b61000f4bd77e619ebf3fee943d0e65bcfc55ef
SilverBlack7 <82745126+SilverBlack7@users.noreply.github.com>
2021-04-18 11:43:32 +0400

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href='http://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+	<div class="header">
+		<a href="/">Cybersecurity capstone Project</a>
+	</div>
+
+		<h1>Please Login or Register</h1>
+		<a href="login.php">Login</a> or
+		<a href="register.php">Register</a>
+
+
+</body>
+</html>
