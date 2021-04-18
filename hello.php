385fcad225e69fa16750537bdca912e5911cadf7
SilverBlack7 <82745126+SilverBlack7@users.noreply.github.com>
2021-04-18 11:48:10 +0400

    Update hello.php

--- hello.php
+++ hello.php
@@ -3,18 +3,13 @@
 <html>
 <head>
 	<title>Home</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<link href='http://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
+
 </head>
 <body>
 
-	<div class="header">
-		<a href="/">Cybersecurity capstone Project</a>
-	</div>
-
-		<h1>Please Login or Register</h1>
-		<a href="login.php">Login</a> or
-		<a href="register.php">Register</a>
+<?php
+	echo "hello";
+?>
 
 
 </body>
